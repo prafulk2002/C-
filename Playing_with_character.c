143dd949902a3089a63efd40e996ad58fbd274e9
Praful Kumar <91059790+prafulk2002@users.noreply.github.com>
2021-10-06 18:44:32 +0530

    Add files via upload

---
+++ Playing_with_character.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch,s[100] ;
+    scanf("%c",&ch);
+    printf("%c\n",ch);
+    scanf("%s",s);
+    printf("%s\n",s);
+    scanf("\n");
+    scanf("%[^\n]%*c", s);
+    printf("%s",s);    
+    return 0;
+}
